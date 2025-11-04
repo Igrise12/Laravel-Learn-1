b356e5cca0b4eac5e24c87f8d6070fcdc6e9f670
Igrise12 <patrickmarcel65@gmail.com>
2025-11-05 00:10:38 +0700

    Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>About Us</h1>
+    <h3>Name : {{ $name }}</h3>
+    <p>Welcome to the about page. Here you can learn more about our website and its purpose.</p>
+</body>
+</html>
