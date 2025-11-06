83fca5f0bc4552c839c66736c70c4e9c9d4223df
Igrise12 <patrickmarcel65@gmail.com>
2025-11-06 09:10:18 +0700

    Commit

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class ="font-bold tracking-tight text-white">Welcome to the blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,11 +12,32 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page', 'name' => 'John Doe', 'email' => 'john@example.com']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        ['id' => 1, 'slug' => 'first-post', 'title' => 'First Post', 'author' => 'Alice', 'body' => 'This is the content of the first post.'],
+        ['id' => 2, 'slug' => 'second-post', 'title' => 'Second Post', 'author' => 'Bob', 'body' => 'This is the content of the second post.'],
+        ['id' => 3, 'slug' => 'third-post', 'title' => 'Third Post', 'author' => 'Charlie', 'body' => 'This is the content of the third post.'],
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug){
+    $posts =[
+        ['id' => 1, 'slug' => 'first-post', 'title' => 'First Post', 'author' => 'Alice', 'body' => 'This is the content of the first post.'],
+        ['id' => 2, 'slug' => 'second-post', 'title' => 'Second Post', 'author' => 'Bob', 'body' => 'This is the content of the second post.'],
+        ['id' => 3, 'slug' => 'third-post', 'title' => 'Third Post', 'author' => 'Charlie', 'body' => 'This is the content of the third post.'],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
 });
+
+
 
