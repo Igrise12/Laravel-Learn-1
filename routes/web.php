cf3017047861996fee986b4a04b93524512170fa
Igrise <patrickmarcel65@gmail.com>
2025-11-07 19:45:34 +0700

    Continue Tutorial

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -25,15 +26,13 @@ Route::get('/posts', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post){
  
-
     return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
 
-Route::get('/authors/{user}', function (User $user){
+Route::get('/authors/{user:username}', function (User $user){
  
-
-    return view('posts', ['title' => 'Article by ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
@@ -41,4 +40,7 @@ Route::get('/contact', function () {
 });
 
 
-
+Route::get('/categories/{category:slug}', function (Category $category){
+ 
+    return view('posts', ['title' => 'Posts in Category: ' . $category->name, 'posts' => $category->posts]);
+});
