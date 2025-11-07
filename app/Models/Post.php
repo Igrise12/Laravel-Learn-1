17c7bf05ce566e3fbf9c705bf3d2b4994ae78998
Igrise12 <patrickmarcel65@gmail.com>
2025-11-07 11:31:00 +0700

    Commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,13 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model 
 
 {
     // ...existing code...
+    use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
 
 };
